d0d4d1078833b21ee740171388049aea8394a0ae
Amber <qe-d@163.com>
2017-12-27 23:19:32 +0800

    ..

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 let db = require('./api/db.js')
 let emit = require('./bus')
 let server = require('./server')
+let express = require('express')
+let http = require('http')
 
 emit.addListener('success', function () {
     console.log('hear success')
 })
 
+
+var server = new http.Server()
+
+server.on('request', (req, res) =>{
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.end('server is running');
+})
+
+server.listen(8080)

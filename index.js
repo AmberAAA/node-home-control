cd57540194b47963babbb0eee82068b293c72e05
AmberAAA <qe-d@163.com>
2018-01-02 17:49:22 +0800

    index

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ emit.addListener('success', function () {
 express.static('/public')
 
 let app = express()
-app.all('/api', function(req, res, next) {
+app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "X-Requested-With");

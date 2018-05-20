30ea132c672954c429f1e64065c257ccde402463
AmberAAA <qe-d@163.com>
2018-05-20 17:05:05 +0800

    .

--- bus/io.js
+++ bus/io.js
@@ -1,12 +1,12 @@
 let bus = require('./index')
 let io = require('socket.io')(require('../config').socketPart)
 
-io.on('connection',function (socket) {
-    console.log('on connection')
+io.on('connection', function (socket) {
+  console.log('on connection')
 
-    bus.on('light', function () {
-        console.log('hear light')
-        socket.emit('light')
-    })
+  bus.on('light', function () {
+    console.log('hear light')
+    socket.emit('light')
+  })
 
 })

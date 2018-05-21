d2d791b894927c2d62403c9dbeb819ee2b02c399
AmberAAA <qe-d@163.com>
2018-05-21 18:52:36 +0800

    add secretKey

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     DB_CONN_SRT: 'mongodb://root:Superadmin123@104.225.237.158:27017/home',
-    socketPart: '8081'
+    socketPart: '8081',
+    secretKey: '8zhw1eJy5unFDPLtnDCx4FL6BPZ3v9hN'
 }
